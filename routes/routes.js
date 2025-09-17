8d4f5b4da07cd64b37f591b00b1be4bf8ca38439
valeria <vale_cerv@outlook.com>
2025-09-17 12:46:08 -0600

    conexion de turnos a la base de datos

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,6 @@ const router = express.Router();
 
 // Importar router de autenticación
 router.use("/auth", require("../src/core/auth/auth.router")); // rutas de autenticacion
-
 // RUTA PARA AREAS
 router.use("/catalogos/areas", require("../src/catalogos/areas/areas.router")); // AREAS
 
@@ -14,5 +13,8 @@ router.use("/catalogos/servicios", require("../src/catalogos/servicios/servicios
 // RUTA PARA USUARIOS
 router.use("/catalogos/usuarios", require("../src/catalogos/usuarios/usuarios.router")); // USUARIOS
 
-// ROUTER DE SECCIONES  
+// ROUTER DE SECCIONES 
+
+router.use("/operaciones/turnos", require("../src/operaciones/turnos/turnos.routes")); // rutas de autenticacion
+
 module.exports = router;
