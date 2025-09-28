d9a1337e2834a6c4098d852b2644b536fd8ee29b
fer463-web <mzepeda22@ucol.mx>
2025-09-27 22:15:45 -0600

    rutas para recuperar contraseña
    Co-Authored-By: quinterodevelopweb <229812711+quinterodevelopweb@users.noreply.github.com>

--- src/core/auth/auth.router.js
+++ src/core/auth/auth.router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { loginUsuario, registerUsuario, logoutUsuario, protectedInUsuario, refreshToken } = require("./auth.controller");
+const passwordController = require("./password.controller");
 const authMiddleware = require("../../../middlewares/authMiddleware");
 
 router.post("/login", loginUsuario);
@@ -8,5 +9,10 @@ router.post("/register", registerUsuario);
 router.post("/logout", logoutUsuario);
 router.post("/protected", authMiddleware, protectedInUsuario);
 router.post("/refresh-token", refreshToken);
+
+//Rutas de recuperación de contraseña
+router.post("/forgot-password", passwordController.forgotPassword);
+router.post("/verify-code", passwordController.verifyCode);
+
 
 module.exports = router;
